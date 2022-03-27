05ca7b6b05e30b5c2a4896d35f0158c94c340fd6
Andrew Glenn <29951057+andrew-glenn@users.noreply.github.com>
2022-03-27 17:50:18 -0500

    Surgical edits of user-agent for awscc module definition

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"os"
-
 	"github.com/hashicorp/hcl/v2"
+	"os"
 	// "github.com/hashicorp/hcl/v2/hclsimple"
 	"flag"
 	"io/ioutil"
@@ -39,7 +39,79 @@ type ModuleVersionProvider struct {
 }
 
 type HCLFile struct {
-	Module []HCLModule `hcl:"module,block"`
+	Module    []HCLModule   `hcl:"module,block"`
+	Providers []HCLProvider `hcl:"provider,block"`
+}
+
+type HCLProvider struct {
+	Name      string
+	UserAgent ProviderUserAgent
+	Body      *hclwrite.Body
+}
+
+type ProviderUserAgent struct {
+	ProductName    TokenSurgeryReference
+	ProductVersion TokenSurgeryReference
+	Comment        TokenSurgeryReference
+	og_tokens      hclwrite.Tokens
+}
+
+func (m *ProviderUserAgent) GenerateNewTokens() hclwrite.Tokens {
+	var modified hclwrite.Tokens
+	for idx, token := range m.og_tokens {
+		switch idx {
+		case m.ProductName.Index:
+			modified = append(modified, m.ProductName.Token)
+		case m.ProductVersion.Index:
+			modified = append(modified, m.ProductVersion.Token)
+		case m.Comment.Index:
+			modified = append(modified, m.Comment.Token)
+		default:
+			modified = append(modified, token)
+		}
+	}
+	return modified
+}
+
+type TokenSurgeryReference struct {
+	Index int
+	Token *hclwrite.Token
+	Value string
+}
+
+func (tsr *TokenSurgeryReference) NewValue(s string) {
+	tsr.Value = s
+	tsr.Token.Bytes = []byte(s)
+}
+
+func UAFromTokens(tokens hclwrite.Tokens) ProviderUserAgent {
+	var ident bool = false
+	var IdentToken *hclwrite.Token
+	token_indents := make(map[string]TokenSurgeryReference)
+	for idx, t := range tokens {
+		if t.Type == hclsyntax.TokenIdent {
+			ident = true
+			IdentToken = t
+			continue
+		}
+		if (t.Type == hclsyntax.TokenQuotedLit) && (ident == true) {
+			ident_str := string(IdentToken.Bytes)
+			value_str := string(t.Bytes)
+			token_indents[ident_str] = TokenSurgeryReference{
+				Index: idx,
+				Token: t,
+				Value: value_str,
+			}
+			ident = false
+		}
+	}
+	ua := ProviderUserAgent{
+		ProductName:    token_indents["product_name"],
+		ProductVersion: token_indents["product_version"],
+		Comment:        token_indents["comment"],
+		og_tokens:      tokens,
+	}
+	return ua
 }
 
 type HCLModule struct {
@@ -83,6 +155,20 @@ func CreateModule(block *hclwrite.Block) HCLModule {
 		Version: ver,
 		Block:   block,
 	}
+}
+
+func CreateProvider(block *hclwrite.Block) (HCLProvider, error) {
+	provider := block.Labels()[0]
+	user_agent := block.Body().GetAttribute("user_agent")
+	if user_agent == nil {
+		return HCLProvider{}, errors.New("No useragent")
+	}
+	ff := UAFromTokens(user_agent.BuildTokens(nil))
+	return HCLProvider{
+		Name:      provider,
+		UserAgent: ff,
+		Body:      block.Body(),
+	}, nil
 }
 
 func WalkMatch(root string, pattern string) ([]string, error) {
@@ -155,12 +241,25 @@ func main() {
 			panic(err)
 		}
 		var modules []HCLModule
+		var providers []HCLProvider
+
 		hf, _ := hclwrite.ParseConfig(data, path, hcl.InitialPos)
+
 		for _, v := range hf.Body().Blocks() {
 			if v.Type() == "module" {
 				modules = append(modules, CreateModule(v))
 			}
+			if v.Type() == "provider" {
+				ua := v.Body().GetAttribute("user_agent")
+				if ua != nil {
+					pv, err := CreateProvider(v)
+					if err == nil {
+						providers = append(providers, pv)
+					}
+				}
+			}
 		}
+
 		for _, b := range modules {
 			var inFamilyVersions []*semver.Version
 			if b.Local() {
@@ -183,6 +282,19 @@ func main() {
 			fmt.Printf("Source: %s, Current pinned Version: %s, Will be upgraded to: %s\n", b.Source, b.Version, proposedVersion.String())
 			changes = true
 			SetVersionAttribute(b.Block, proposedVersion)
+		}
+		//if changes {
+		//	os.WriteFile(path, hclwrite.Format(hf.Bytes()), 0644)
+		//}
+
+		for _, m := range providers {
+			if m.Name == "awscc" {
+				m.UserAgent.ProductName.NewValue("blah")
+				m.UserAgent.ProductVersion.NewValue("0.9.1")
+				NewTokens := m.UserAgent.GenerateNewTokens()
+				m.Body.SetAttributeRaw("user_agent", NewTokens[2:])
+				changes = true
+			}
 		}
 		if changes {
 			os.WriteFile(path, hclwrite.Format(hf.Bytes()), 0644)
